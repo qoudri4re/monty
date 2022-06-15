6634ff091f2cda0da86a3429c335edb29ae43897
qoudri4re <63318729+qoudri4re@users.noreply.github.com>
2022-06-15 10:46:37 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -17,7 +17,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
@@ -32,7 +32,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct instruction_s
 {
